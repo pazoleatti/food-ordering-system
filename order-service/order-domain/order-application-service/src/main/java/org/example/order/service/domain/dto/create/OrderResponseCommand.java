a6abf5c1f4695d47cebb79d35ceea92325fee69a
ytrofimov <Yuriy.Trofimov@aplana.com>
2022-10-17 21:44:57 +0300

    18. Implementing Order Application Service - DTO classes

---
+++ order-service/order-domain/order-application-service/src/main/java/org/example/order/service/domain/dto/create/OrderResponseCommand.java
@@ -0,0 +1,21 @@
+package org.example.order.service.domain.dto.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.domain.valueobject.OrderStatus;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class OrderResponseCommand {
+    @NotNull
+    private final UUID orderTrackingId;
+    @NotNull
+    private final OrderStatus orderStatus;
+    @NotNull
+    private final String message;
+}
