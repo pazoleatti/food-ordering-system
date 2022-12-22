488aaf039dd0e5c1f063cced060c4049a3ab023d
ytrofimov <Yuriy.Trofimov@aplana.com>
2022-12-22 20:01:54 +0300

    44. Domain core module: Adding Exception and Domain events

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/org/example/payment/service/domain/event/PaymentCancelledEvent.java
@@ -0,0 +1,20 @@
+package org.example.payment.service.domain.event;
+
+import org.example.domain.event.publisher.DomainEventPublisher;
+import org.example.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+
+public class PaymentCancelledEvent extends PaymentEvent {
+
+    private final DomainEventPublisher<PaymentCancelledEvent> paymentCancelledEventDomainEventPublisher;
+
+    public PaymentCancelledEvent(Payment payment,
+                                 ZonedDateTime createdAt,
+                                 DomainEventPublisher<PaymentCancelledEvent>
+                                         paymentCancelledEventDomainEventPublisher) {
+        super(payment, createdAt, Collections.emptyList());
+        this.paymentCancelledEventDomainEventPublisher = paymentCancelledEventDomainEventPublisher;
+    }
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/org/example/payment/service/domain/event/PaymentFailedEvent.java
@@ -0,0 +1,20 @@
+package org.example.payment.service.domain.event;
+
+import org.example.domain.event.publisher.DomainEventPublisher;
+import org.example.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class PaymentFailedEvent extends PaymentEvent {
+
+    private final DomainEventPublisher<PaymentFailedEvent> paymentFailedEventDomainEventPublisher;
+
+    public PaymentFailedEvent(Payment payment,
+                              ZonedDateTime createdAt,
+                              List<String> failureMessages,
+                              DomainEventPublisher<PaymentFailedEvent> paymentFailedEventDomainEventPublisher) {
+        super(payment, createdAt, failureMessages);
+        this.paymentFailedEventDomainEventPublisher = paymentFailedEventDomainEventPublisher;
+    }
+}
