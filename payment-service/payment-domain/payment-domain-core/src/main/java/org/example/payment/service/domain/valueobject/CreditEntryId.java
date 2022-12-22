af12e33b228a4e3cfc88fbfa238a9665d1bc2c35
ytrofimov <Yuriy.Trofimov@aplana.com>
2022-12-22 19:41:25 +0300

    43. Domain core module: Adding Aggregate Root, Entity and Value objects

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/org/example/payment/service/domain/valueobject/CreditEntryId.java
@@ -0,0 +1,11 @@
+package org.example.payment.service.domain.valueobject;
+
+import org.example.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class CreditEntryId extends BaseId<UUID> {
+    public CreditEntryId(UUID value) {
+        super(value);
+    }
+}
