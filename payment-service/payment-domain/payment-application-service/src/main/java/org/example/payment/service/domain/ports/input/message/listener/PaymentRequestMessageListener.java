bde70e723332a7c76b6858ca4f43619647d4294a
ytrofimov <Yuriy.Trofimov@aplana.com>
2022-12-26 20:59:31 +0300

    46. Application Service domain module: Adding Mapper, Config and Ports

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/org/example/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
@@ -0,0 +1,10 @@
+package org.example.payment.service.domain.ports.input.message.listener;
+
+import org.example.payment.service.domain.dto.PaymentRequest;
+
+public interface PaymentRequestMessageListener {
+
+    void completePayment(PaymentRequest paymentRequest);
+
+    void cancelPayment(PaymentRequest paymentRequest);
+}
---
+++ payment-service/payment-domain/payment-application-service/src/main/java/org/example/payment/service/domain/ports/output/repository/CreditEntryRepository.java
@@ -0,0 +1,13 @@
+package org.example.payment.service.domain.ports.output.repository;
+
+import org.example.domain.valueobject.CustomerId;
+import org.example.payment.service.domain.entity.CreditEntry;
+
+import java.util.Optional;
+
+public interface CreditEntryRepository {
+
+    CreditEntry save(CreditEntry creditEntry);
+
+    Optional<CreditEntry> findByCustomerId(CustomerId customerId);
+}
