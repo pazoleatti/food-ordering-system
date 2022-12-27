3059e38573583a0bdab59957f1261e85993195eb
ytrofimov <Yuriy.Trofimov@aplana.com>
2022-12-27 19:18:45 +0300

    48. Application Service domain module: Implementing input ports - Part 2

--- payment-service/payment-domain/payment-application-service/src/main/java/org/example/payment/service/domain/PaymentRequestMessageListenerImpl.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/org/example/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -18,20 +18,20 @@ public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageL
 
     @Override
     public void completePayment(PaymentRequest paymentRequest) {
-      /*  PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
-        fireEvent(paymentEvent);*/
+        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
+        fireEvent(paymentEvent);
     }
 
     @Override
     public void cancelPayment(PaymentRequest paymentRequest) {
-        /*PaymentEvent paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest);
-        fireEvent(paymentEvent);*/
+        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest);
+        fireEvent(paymentEvent);
     }
 
-   /* private void fireEvent(PaymentEvent paymentEvent) {
+    private void fireEvent(PaymentEvent paymentEvent) {
         log.info("Publishing payment event with payment id: {} and order id: {}",
                 paymentEvent.getPayment().getId().getValue(),
                 paymentEvent.getPayment().getOrderId().getValue());
-        paymentEvent.fire();
-    }*/
+        /*paymentEvent.fire();*/
+    }
 }
